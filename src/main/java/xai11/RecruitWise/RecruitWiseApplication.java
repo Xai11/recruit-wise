ee27e6db018a4e2d311c1435df59c581f3a8d904
Xai11 <sashakochetov300@gmail.com>
2024-10-04 23:24:50 +0300

    initialization from Spring IO

---
+++ src/main/java/xai11/RecruitWise/RecruitWiseApplication.java
@@ -0,0 +1,13 @@
+package xai11.RecruitWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitWiseApplication.class, args);
+	}
+
+}
